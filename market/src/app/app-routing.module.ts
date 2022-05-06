e788c3f240d7a3d770e6431bec361368c7d9d5b1
baucicmario <baucicmario2.6@gmail.com>
2022-05-06 16:09:53 +0200

    updated navigation and added regist form

--- market/src/app/app-routing.module.ts
+++ market/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CimformComponent } from './cimform/cimform.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path:'home',component:HomeComponent }
+  {path:'home', component:HomeComponent},
+  {path:'cimform', component:CimformComponent}
+  
 ];
 
 @NgModule({
---
+++ market/src/app/cimform/cimform.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cimform',
+  templateUrl: './cimform.component.html',
+  styleUrls: ['./cimform.component.css']
+})
+export class CimformComponent {
+  addressForm = this.fb.group({
+    company: null,
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    address: [null, Validators.required],
+    address2: null,
+    city: [null, Validators.required],
+    state: [null, Validators.required],
+    postalCode: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
+    ],
+    shipping: ['free', Validators.required]
+  });
+
+  hasUnitNumber = false;
+
+  states = [
+    {name: 'Bács-Kiskun', abbreviation: 'AL'},
+    {name: 'Baranya', abbreviation: 'AK'},
+    {name: 'Békés', abbreviation: 'AS'},
+    {name: 'Borsod-Abaúj-Zemplén', abbreviation: 'AZ'},
+    {name: 'Csongrád-Csanád', abbreviation: 'AR'},
+    {name: 'Fejér', abbreviation: 'CA'},
+    {name: 'Győr-Moson-Sopron', abbreviation: 'CO'},
+    {name: 'Hajdú-Bihar', abbreviation: 'CT'},
+    {name: 'Heves', abbreviation: 'DE'},
+    {name: 'Jász-Nagykun-Szolnok', abbreviation: 'DC'},
+    {name: 'Komárom-Esztergom', abbreviation: 'FM'},
+    {name: 'Nógrád', abbreviation: 'FL'},
+    {name: 'Pest', abbreviation: 'GA'},
+    {name: 'Somogy', abbreviation: 'GU'},
+    {name: 'Szabolcs-Szatmár-Bereg', abbreviation: 'HI'},
+    {name: 'Tolna', abbreviation: 'ID'},
+    {name: 'Vas', abbreviation: 'IL'},
+    {name: 'Veszprém', abbreviation: 'IN'},
+    {name: 'Zala', abbreviation: 'IA'}
+   
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    alert('Thanks!');
+  }
+}
